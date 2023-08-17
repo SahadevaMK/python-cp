6b5db25dd02b0789adc0d4a997767a421703e953
SahadevaMK <sahadevamk121@gmail.com>
2023-08-18 01:58:46 +0530

    init

---
+++ Majority Element.py
@@ -0,0 +1,14 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        res = {}
+        for i in nums:
+            if i in res:
+                res[i]+=1
+            else:
+                res[i]=1
+        maxim = max(res.values())
+        if (maxim > (len(nums)//2)):
+            for i in res:
+                if res[i] == maxim:
+                    return i
+        return -1
